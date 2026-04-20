d99cd157b258581151d8e93593cec6dbeea01d14
Dharm Lakum <dalakum1112@gmail.com>
2026-04-20 14:01:48 +0530

    V2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isVercel = process.env.VERCEL === "1";
-
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  ...(isVercel ? {} : { distDir: "next-build" }),
   images: {
     formats: ["image/avif", "image/webp"],
   },
